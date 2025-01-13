740850a1bf4cf700f0b83116a7d421b7dd73d053
ZamzamF <zamzam.fahiyee@outlook.com>
2025-01-13 14:27:54 +0100

    Form styled

--- client/script.js
+++ client/script.js
@@ -1,32 +1,6 @@
 //Global variabel för bas-urlen : /furniture
 const baseUrl = "http://localhost:3000/furniture";
 
-/* Formulär - Gamla
-  document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('resourceForm');
-
-    form.addEventListener('submit', (e) => {
-      e.preventDefault(); 
-
-      // Hämta data från formuläret
-      const formData = {
-        furnitureName: document.getElementById('furnitureName').value,
-        color: document.getElementById('color').value,
-        category: document.getElementById('category').value,
-        price: document.getElementById('price').value,
-      };
-
-      console.log('Formulärdata:', formData);
-
-      // Rensa formuläret 
-      alert('Formuläret skickades!');
-      form.reset();
-      const modal = bootstrap.Modal.getInstance(document.getElementById('resourceFormModal'));
-      modal.hide();
-    });
-  });
-*/
-
 
 
 // ------------------- Dynamiskt rendering av produkter ---------
@@ -48,13 +22,13 @@ async function fetchAllProducts(){
     //Renderar ut prdukterna 
     fetchedProducts.forEach((product) => {
         const productCards = `
-        <div class="m-10 w-80 rounded-lg border border-gray-200 bg-amber-50 py-6 px-6 shadow-lg">  
+        <div class="m-10 w-80 rounded-lg border border-gray-200 bg-amber-50 py-3 px-3 shadow-lg">  
             <p class="text-lg font-bold text-green-800">${product.furnitureName}</p>
             <p class="text-sm font-semibold text-green-700">Kategori: ${product.category}</p>
-            <p class="mt-3 text-2xl font-bold text-amber-900">${product.price}</p>
+            <p class="mt-3 text-1xl font-bold text-amber-900">${product.price}</p>
             <p class="text-sm font-semibold text-green-600">Färg: ${product.color}</p>
 
-            <div class="mt-6 flex justify-between">
+            <div class="mt-4 flex justify-between">
                 <button class="w-1/2 rounded-lg border-2 border-green-700 bg-green-700 px-4 py-2 text-sm font-semibold text-white hover:bg-green-800">
                 Ändra
                 </button>
